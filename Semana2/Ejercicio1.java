8c38de870d37fae0ca3bfa03878ed62afe515220
Atulx31 <Vidaldavid132@gmail.com>
2026-02-10 16:32:12 +0000

    se agregaron 2 numeros al arreglo

--- Semana2/Ejercicio1.java
+++ Semana2/Ejercicio1.java
@@ -2,7 +2,7 @@ package Semana2;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        int []a={1,2,3,4,5,6};
+        int []a={1,2,3,4,5,6,8,9};
 
         for (int i = 0; i < a.length; i++) {
             System.out.println("a[" + i + "] = " + a[i]);
