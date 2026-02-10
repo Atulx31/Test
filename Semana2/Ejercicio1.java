05c5327ae841e862737a3ca150d43129d7393663
Atulx31 <Vidaldavid132@gmail.com>
2026-02-10 16:27:57 +0000

    Primer ejercicio con arreglos

---
+++ Semana2/Ejercicio1.java
@@ -0,0 +1,11 @@
+package Semana2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int []a={1,2,3,4,5,6};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+    }
+}
