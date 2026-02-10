a30c9e1e18546cb9d525cdd62e31bf5fada2d162
Atulx31 <Vidaldavid132@gmail.com>
2026-02-10 16:51:49 +0000

    ahora el programa suma los pares e impares del arreglo

--- Semana2/Ejercicio1.java
+++ Semana2/Ejercicio1.java
@@ -2,10 +2,19 @@ package Semana2;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        int []a={1,2,3,4,5,6,8,9};
-
+        int []a={4,8,9,6,1,2};
+        int pares = 0;
+        int impares = 0;
         for (int i = 0; i < a.length; i++) {
             System.out.println("a[" + i + "] = " + a[i]);
+            if (a[i] % 2 == 0) {
+                pares += a[i];
+            } else {
+                impares += a[i];
+            }
+        }
+        System.out.println("Suma de números pares: " + pares);
+        System.out.println("Suma de números impares: " + impares);   
         }
     }
-}
+
