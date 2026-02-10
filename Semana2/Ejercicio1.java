cefdca15f28bed84ede2b9e51f7480f82443eb14
Atulx31 <Vidaldavid132@gmail.com>
2026-02-10 17:29:30 +0000

    forma 2 del arreglo

--- Semana2/Ejercicio1.java
+++ Semana2/Ejercicio1.java
@@ -5,8 +5,12 @@ public class Ejercicio1 {
         int []a={4,8,9,6,1,2};
         int pares = 0;
         int impares = 0;
+
+        //iterar los elementos del arreglo
         for (int i = 0; i < a.length; i++) {
             System.out.println("a[" + i + "] = " + a[i]);
+
+            //suma los números pares e impares
             if (a[i] % 2 == 0) {
                 pares += a[i];
             } else {
@@ -14,7 +18,11 @@ public class Ejercicio1 {
             }
         }
         System.out.println("Suma de números pares: " + pares);
-        System.out.println("Suma de números impares: " + impares);   
+        System.out.println("Suma de números impares: " + impares);  
+        
+        //Forma 2
+
+        int []v = new int[10];
         }
     }
 
